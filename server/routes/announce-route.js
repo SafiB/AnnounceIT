c0f720329e8045a838cb5a925976a7bcc5ef67ea
SafiB <nusego4@yahoo.fr>
2020-01-29 11:47:37 +0200

    improve reset password pages, correct announcement route

--- server/routes/announce-route.js
+++ server/routes/announce-route.js
@@ -10,12 +10,12 @@ import check from '../middlewares/user-checker';
 
 const routersannounce = express.Router();
 routersannounce.post('/api/v1/announcement', authentic, validate, duplication, announcementController.create);
-routersannounce.patch('/api/v1/updatedAnnouncement/:id', authentic, authorize, validate, announcementController.updatedAnnouncement);
-routersannounce.get('/api/v1/allannouncements', authentic, announcementController.all);
-routersannounce.get('/api/v1/announcementstatus', authentic, announcementController.findByStatus);
-routersannounce.get('/api/v1/specificannouncement/:id', authentic, authorize, announcementController.getAnnouncement);
+routersannounce.patch('/api/v1/Announcement/:id', authentic, authorize, validate, announcementController.update);
+routersannounce.get('/api/v1/announcement', authentic, announcementController.all);
+routersannounce.get('/api/v1/announcement', authentic, announcementController.findByStatus);
+routersannounce.get('/api/v1/announcement/:id', authentic, authorize, announcementController.getAnnouncement);
 routersannounce.delete('/api/v1/announcement/:id', authentic, check, announcementController.delete);
-routersannounce.patch('/api/v1/announcements/:id', authentic, check, announcementController.changeStatus);
-routersannounce.get('/api/v1/announcements/', authentic, check, announcementController.allAnnouncements);
+routersannounce.patch('/api/v1/announcement/:id', authentic, check, announcementController.changeStatus);
+routersannounce.get('/api/v1/announcement/', authentic, check, announcementController.allAnnouncements);
 
 export default routersannounce;
